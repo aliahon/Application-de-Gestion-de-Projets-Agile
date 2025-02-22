30abff668c846f3d427abcde749b61017616532c
EL IDRISSI Nohaila <nohaila09el@gmail.com>
2025-02-22 12:29:50 +0100

    initializing the project

---
+++ src/test/java/com/GestionProjet/GestionProjet/GestionProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionProjet.GestionProjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
